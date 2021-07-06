a0fcf857c290b430714bf75c0d6b5be6109d0897
Nick De Raj <nickderaj@gmail.com>
2021-07-06 16:05:39 +0800

    Defining the template replacing function

--- index.js
+++ index.js
@@ -33,6 +33,22 @@ console.log("Will read file!");
 
 //////////////////////////////////
 // SERVER //
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName); // the /NAME/g replaces ALL (global) not just the first one
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+  output = output.replace(/{%NAME%}/g, product.name);
+
+  if (!product.organic)
+    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+  return output;
+};
+
 const tempOverview = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
   "utf-8"
@@ -54,6 +70,9 @@ const server = http.createServer((req, res) => {
   // Overview page
   if (pathName === "/" || pathName === "/overview") {
     res.writeHead(200, { "Content-type": "text/html" });
+
+    const cardsHtml = dataObj.map((el) => replaceTemplate(tempCard, el));
+
     res.end(tempOverview);
 
     // Product page
